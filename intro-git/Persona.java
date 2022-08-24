b1edb2b1608db3ee706912a6766c57b513dafb91
debnix <daquipe5@gmail.com>
2022-08-24 18:29:58 -0500

    refactor

---
+++ intro-git/Persona.java
@@ -0,0 +1,17 @@
+public class Persona {
+	private int id;
+	private String name;
+
+	public Persona(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
