ab5b4472b0efc826a67dd273aa4713cbea5f0117
debnix <daquipe5@gmail.com>
2022-08-24 18:22:22 -0500

    add file intro-git/JDBCUtilities.java

---
+++ intro-git/JDBCUtilities.java
@@ -0,0 +1,13 @@
+public class JDBCUtilities {
+  public JDBCUtilities() {
+
+  }
+
+  public void conectar() {
+    System.out.println("Conectando a la BD...");
+  }
+
+  public void desconectar() {
+    System.out.println("Desconectando de la BD...");
+  }
+}
