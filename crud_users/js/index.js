95ce8cdec22f06cbfa17ed1d7e7c4394e8700b3f
debnix <daquipe5@gmail.com>
2022-08-30 18:47:48 -0500

    introduction-js: add array users into index.js

---
+++ crud_users/js/index.js
@@ -0,0 +1,12 @@
+
+const users = [
+  { foto: "", nombre: "Andrés", apellido: "Quintero", email: "andres@gmail.com", fecha_nacimiento: "10/08/1993" },
+  { foto: "", nombre: "Alejandra", apellido: "Perea", email: "alejandra@gmail.com", fecha_nacimiento: "120/10/1992" },
+  { foto: "", nombre: "Daniel", apellido: "Perez", email: "daniel@gmail.com", fecha_nacimiento: "15/09/1990" },
+  { foto: "", nombre: "Laura", apellido: "Hernandez", email: "laura@gmail.com", fecha_nacimiento: "25/11/1995" }
+]
+
+for (let i = 0; i < users.length; i++) {
+
+}
+
