4e56ce92c0b46d4b4f3b8fe0d1abf3593d67dda9
debnix <daquipe5@gmail.com>
2022-09-06 18:49:49 -0500

    Create session Object and begin transaction into App.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,6 +1,9 @@
 package com.hibernate.introduction;
 
 import com.hibernate.introduction.modelo.Persona;
+
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,10 +11,29 @@ import org.hibernate.cfg.Configuration;
 public class App {
     public static void main(String[] args) {
         // Crear objeto para el manejo de la sesión de la BD
-        SessionFactory secionFactory = new Configuration()
+        SessionFactory sessionFactory = new Configuration()
                 .configure("cfg.xml")
                 .addAnnotatedClass(Persona.class)
                 .buildSessionFactory();
         System.out.println("Hola mundo");
+
+        // Abrir Sesión
+        Session session = sessionFactory.openSession();
+
+        // ...Generar transacciones
+        try {
+            // Crear fecha
+            Date fecha_nacimiento = new Date();
+            // Crear Objeto
+            Persona persona = new Persona(1, "Andres", "Quintero", "andres@gmail.com", fecha_nacimiento,
+                    "http://fake-url");
+            // Preparar sesión para generar transacciones
+            session.beginTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Cerrar Sesión
+        session.close();
     }
 }
