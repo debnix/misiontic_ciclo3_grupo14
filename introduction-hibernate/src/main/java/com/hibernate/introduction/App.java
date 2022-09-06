6253598bd6da13c0966b24f009d6ecc43eeb91bd
debnix <daquipe5@gmail.com>
2022-09-06 18:35:14 -0500

    add SessionFactory into App.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,13 +1,17 @@
 package com.hibernate.introduction;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.hibernate.introduction.modelo.Persona;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        // Crear objeto para el manejo de la sesión de la BD
+        SessionFactory secionFactory = new Configuration()
+                .configure("cfg.xml")
+                .addAnnotatedClass(Persona.class)
+                .buildSessionFactory();
+        System.out.println("Hola mundo");
     }
 }
