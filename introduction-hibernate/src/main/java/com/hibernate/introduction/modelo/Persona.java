b8b408bf183d1f036ca184d6fbed2ca737f4125f
debnix <daquipe5@gmail.com>
2022-09-06 18:16:15 -0500

    add decorators hibernate to Class Persona into src/main/java/com/hibernate/introduction/modelo/Persona.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/modelo/Persona.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/modelo/Persona.java
@@ -2,13 +2,29 @@ package com.hibernate.introduction.modelo;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Indicar que la Clase representa una entidad en BD
+@Entity
+// Nombre de la tabla/entidad a la que representa la Clase
+@Table(name = "users")
 public class Persona {
-  // ATRIBUTOS
+  // ATRIBUTOS - CAMPOS EN BD
+  @Id
+  @Column(name = "id")
   private int id;
+  @Column(name = "nombres")
   private String nombres;
+  @Column(name = "apellidos")
   private String apellidos;
+  @Column(name = "email")
   private String email;
+  @Column(name = "fecha_nacimiento")
   private Date fecha_nacimiento;
+  @Column(name = "foto")
   private String foto;
 
   // CONSTRUCTOR
