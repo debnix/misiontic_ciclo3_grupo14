a6c6e1565175ea90ad884ea390e52f22d9045b34
debnix <daquipe5@gmail.com>
2022-09-13 18:25:31 -0500

    crud_users: call api rest introduction-spring-boot

--- crud_users/js/index.js
+++ crud_users/js/index.js
@@ -13,15 +13,16 @@ function mostrar_usuarios (users) {
   let tbody = '<tbody>'
   for (let i = 0; i < users.length; i++) {
     const obj = users[i]
+    const arrayDate = obj.fecha_nacimiento.split("T")
     tbody += `
     <tr>
       <td>
       <img width="120" src="${obj.foto}"/>
       </td>
-      <td>${obj.nombre}</td>
-      <td>${obj.apellido}</td>
+      <td>${obj.nombres}</td>
+      <td>${obj.apellidos}</td>
       <td>${obj.email}</td>
-      <td>${obj.fecha_nacimiento}</td>
+      <td>${arrayDate[0]}</td>
     </tr>
   `
   }
@@ -29,4 +30,17 @@ function mostrar_usuarios (users) {
   tabla.innerHTML += tbody;
 }
 
-mostrar_usuarios(users)
+async function get_all_users (url) {
+  // Enviar petición al servidor
+  const resp = await fetch(url)
+  const users = await resp.json()
+  return users
+}
+
+async function main () {
+  const url = "http://localhost:8080/personas"
+  const users = await get_all_users(url)
+  mostrar_usuarios(users)
+}
+
+main()
