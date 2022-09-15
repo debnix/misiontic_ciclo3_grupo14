a5a3158a9975d09af28fea9d23b39c958104f001
debnix <daquipe5@gmail.com>
2022-09-14 20:35:04 -0500

    refactor: introduction-spring-boot/target/classes/com/hibernate/introduction/App.class

--- introduction-spring-boot/src/main/java/com/hibernate/introduction/controlador/PersonaController.java
+++ introduction-spring-boot/src/main/java/com/hibernate/introduction/controlador/PersonaController.java
@@ -2,6 +2,7 @@ package com.hibernate.introduction.controlador;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,11 +47,13 @@ public class PersonaController {
   }
 
   @PutMapping
+  @CrossOrigin("*")
   public String updatePersona(@RequestBody Persona persona) {
     return service.updatePersona(persona);
   }
 
   @DeleteMapping("/{id}")
+  @CrossOrigin("*")
   public String deletePersona(@PathVariable(name = "id") int id) {
     return service.deletePersona(id);
   }
