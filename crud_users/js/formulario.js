2f7e393816a389d86379e0d66eaa3e3f09c0b404
debnix <daquipe5@gmail.com>
2022-09-14 19:50:18 -0500

    crud_users:

--- crud_users/js/formulario.js
+++ crud_users/js/formulario.js
@@ -1,5 +1,10 @@
 const URL_API = "http://localhost:8080/personas"
 
+let UPDATE_PERSON = {
+  update: false,
+  id: null
+}
+
 function getDataForm (e) {
   // Indicar al evento que no recargue página
   e.preventDefault()
@@ -11,11 +16,33 @@ function getDataForm (e) {
     fecha_nacimiento: form.fecha_nacimiento.value,
     foto: form.foto.value
   }
-  sendData(persona)
+  if (UPDATE_PERSON.update) {
+    sendDataUpdate(persona)
+  } else {
+    sendDataCreate(persona)
+  }
+
   clearInputs(form)
 }
 
-async function sendData (persona) {
+async function sendDataUpdate (persona) {
+  // Adicionar id al objeto
+  persona.id = UPDATE_PERSON.id
+  // Enviar petición
+  const resp = await fetch(URL_API, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(persona)
+  })
+
+  const text = await resp.text()
+  alert(text)
+  window.location.href = "index.html"
+}
+
+async function sendDataCreate (persona) {
   // Enviar petición
   const resp = await fetch(URL_API, {
     method: 'POST',
@@ -37,9 +64,23 @@ function clearInputs (form) {
 }
 
 function getDataUrl () {
-  const url = new URLSearchParams()
-  const persona = url.getAll("persona")
-  console.log(persona)
+  const search = window.location.search
+  const url = new URLSearchParams(search)
+  const persona = url.get("persona")
+  if (persona) {
+    const objPersona = JSON.parse(persona)
+    const arrayDate = objPersona.fecha_nacimiento.split("T")
+
+    document.getElementById("input_nombre").setAttribute("value", objPersona.nombres)
+    document.getElementById("input_apellido").setAttribute("value", objPersona.apellidos)
+    document.getElementById("input_email").setAttribute("value", objPersona.email)
+    document.getElementById("input_fecha_nacimiento").setAttribute("value", arrayDate[0])
+    document.getElementById("input_foto").setAttribute("value", objPersona.foto)
+
+    UPDATE_PERSON.update = true
+    UPDATE_PERSON.id = objPersona.id
+  }
+
 }
 
 getDataUrl()
